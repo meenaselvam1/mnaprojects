faa40a0708b5a41d38d6fd7de17e494b00dfa097
meenaselvam1 <75545503+meenaselvam1@users.noreply.github.com>
2020-12-29 18:06:00 -0800

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,32 @@
+To run the program, 
+1. Move the javasample.jar and the javaquestionsAns.txt 
+into any folder of your choice.
+2. Then from this folder, issue the command : java -classpath javasample.jar TechTest
+(TechTest is the Main class)
+ 
+The program is a True Or False Quiz.
+ 
+All True or false questions and answers are put in a default text file named 
+javaquestionsAns.txt. User is given an option to use a custom file
+when the program runs.
+
+The file should be placed in the folder from where you 
+are running the program(where the .exe file is kept.)
+Here is the content of a sample javaquestionsAns.txt file.
+
+Interface can have static methods defined
+T
+Interface cannot have defender methods defined
+F
+Abstract class can have some methods implemented
+T
+Abstract class can be instantiated
+F
+You can't force garbage collection but you can request it
+T
+Constructors can return a value
+F
+Casting cannot be performed between objects of different types.
+T
+Java class can be inherited from two classes
+F
---
+++ javatechtest/TechTest.java
@@ -0,0 +1,113 @@
+package com.practicaljava.javatechtest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.NoSuchFileException;
+import java.nio.charset.StandardCharsets;
+import java.util.Vector;
+import javax.swing.JOptionPane;
+
+/*
+ * TechTest Class is the driver class parsing file containing 
+ * questions and answers. Populates the questionSet and answerSet
+ * Vectors. Delegates the display of GUI to the View class.
+ * Shows dialog for errors and exceptions.
+ */
+public class TechTest {
+	static int numberOfQuestions = 0;
+	static int questionNum = 0;
+	static Vector  questionSet = new Vector();
+	static Vector answerSet = new Vector();
+	static boolean noFile = false;
+	static boolean incorrectAnswerFormat = false;
+
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				String questionAnsFileName = readQuestionAnswerFileName();
+				if (!noFile)
+				    parseQuestionAnswerFile(questionAnsFileName);
+				if ((!noFile) &&  (!incorrectAnswerFormat)) {
+					if (!questionSet.isEmpty()) { 
+						View view = new View(); 
+						view.display((String) questionSet.get((questionNum)));
+					}
+					else
+						JOptionPane.showMessageDialog(null,"Questions are missing. Check the " + questionAnsFileName + " file", "Error",JOptionPane.PLAIN_MESSAGE);
+				}
+			}
+		});
+
+	}
+	static String readQuestionAnswerFileName()
+	{
+		String questionAnsFileName = JOptionPane.showInputDialog("Enter Filename with Questions and Answers: Default is javaquestionAns.txt. " +
+				"Click OK to use the default");
+		try {
+			if (questionAnsFileName.isEmpty()) {
+				questionAnsFileName = "javaQuestionsAns.txt";
+				JOptionPane.showMessageDialog(null, "Using the default file:  " + questionAnsFileName, 
+						"Message", JOptionPane.PLAIN_MESSAGE);
+			}
+			else 
+				JOptionPane.showMessageDialog(null, "Using the custom file:  " + questionAnsFileName, 
+						"Message", JOptionPane.PLAIN_MESSAGE);
+
+		}
+		catch (NullPointerException e) {
+			noFile = true;
+		}
+        
+		return questionAnsFileName;
+
+	}
+	// Nonblocking IO (nio) is used
+	static void parseQuestionAnswerFile(String questionAnsFile) {
+
+		Path qAnsFile = Paths.get(questionAnsFile);
+
+		try (BufferedReader reader = 
+				Files.newBufferedReader(qAnsFile, StandardCharsets.UTF_8)){
+			String question;
+			String answer;
+			while (true) {
+				question = reader.readLine();
+				if (question == null) 
+					break;
+				if (!question.isEmpty()) {
+					questionSet.add(question);
+					numberOfQuestions++;
+				}
+				answer = reader.readLine();
+				if (answer == null) 
+					break;
+
+				if (!answer.isEmpty()) {
+					if (( (answer.equals("T"))) || ((answer.equals("F")))) 
+						answerSet.add(answer);
+					else {
+						JOptionPane.showMessageDialog(null,"Answer not in correct format : T or F", 
+								                     "Error ",JOptionPane.PLAIN_MESSAGE);
+						incorrectAnswerFormat = true;
+					}
+				}
+			}
+		}
+		// Catch exceptions while reading the File with questions and answers
+		catch (NoSuchFileException e)
+		{
+			noFile = true;
+			JOptionPane.showMessageDialog(null, questionAnsFile + ": File not found", 
+					                      "Exception",JOptionPane.PLAIN_MESSAGE);
+		}
+		
+		catch (IOException ioe){ 
+			noFile = true;
+			JOptionPane.showMessageDialog(null, "Error while reading " + questionAnsFile + " " + 
+			                              ioe.toString(),"Exception",JOptionPane.PLAIN_MESSAGE);
+		}
+	}
+}
